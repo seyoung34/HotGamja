30cd3d0eaf650e3b78ea6d4727776af49785b2d2
jmc98 <jmc0820@naver.com>
2024-09-20 22:11:49 +0900

    design : 검색 결과 아이템

--- app/src/main/java/com/example/potatoservice/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/potatoservice/ui/home/HomeViewModel.kt
@@ -1,13 +1,14 @@
 package com.example.potatoservice.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-
-	private val _text = MutableLiveData<String>().apply {
-		value = "This is home Fragment"
-	}
-	val text: LiveData<String> = _text
+	var title = "봉사 활동 제목"
+	var organization = "봉사 기관 이름"
+	var category = "봉사 분야"
+	var recruitmentPeriod = "모집 기간"
+	var recruitmentNumber = "모집 인원"
+	var time = "활동 기간"
+	var serviceRecognitionTime = "봉사 인정 시간"
+	var location = "상세 장소"
 }
