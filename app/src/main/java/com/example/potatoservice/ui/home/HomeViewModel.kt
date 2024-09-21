39e6b16cf7c0398b2e63e583c0b8124566a09470
jmc98 <jmc0820@naver.com>
2024-09-21 18:04:57 +0900

    feat : 홈 화면 리사이클러뷰 생성

--- app/src/main/java/com/example/potatoservice/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/potatoservice/ui/home/HomeViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.potatoservice.ui.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
@@ -11,4 +13,36 @@ class HomeViewModel : ViewModel() {
 	var time = "활동 기간"
 	var serviceRecognitionTime = "봉사 인정 시간"
 	var location = "상세 장소"
+	val serviceList:LiveData<List<Service>> = MutableLiveData(
+		listOf(
+			Service(title, organization, category,
+				recruitmentPeriod, recruitmentNumber, time,
+				serviceRecognitionTime, location
+			),
+			Service("title", "organization", "category",
+				"recruitmentPeriod", "recruitmentNumber", "time",
+				"serviceRecognitionTime", "location"
+			),
+			Service("title", "organization", "category",
+				"recruitmentPeriod", "recruitmentNumber", "time",
+				"serviceRecognitionTime", "location"
+			),
+			Service("title", "organization", "category",
+				"recruitmentPeriod", "recruitmentNumber", "time",
+				"serviceRecognitionTime", "location"
+			),
+			Service("title", "organization", "category",
+				"recruitmentPeriod", "recruitmentNumber", "time",
+				"serviceRecognitionTime", "location"
+			),
+			Service("title", "organization", "category",
+				"recruitmentPeriod", "recruitmentNumber", "time",
+				"serviceRecognitionTime", "location"
+			),
+			Service(title, organization, category,
+				recruitmentPeriod, recruitmentNumber, time,
+				serviceRecognitionTime, location
+			)
+		)
+	)
 }
