3ce3f60ace8b32f11f9f60de320f7c70fee1ec14
seyoung34 <jupi0304@naver.com>
2024-09-24 10:33:30 +0900

    mypage 작업 옮겨오기, viewModel, VolunteerAdapter

--- app/src/main/java/com/example/potatoservice/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/potatoservice/ui/mypage/MyPageFragment.kt
@@ -1,5 +1,38 @@
 package com.example.potatoservice.ui.mypage
 
-class MyPageFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.potatoservice.databinding.FragmentMypageBinding
 
+class MyPageFragment : Fragment() {
+
+    private  lateinit var binding: FragmentMypageBinding
+//    private  lateinit var voluntieerAdapter :
+    private  lateinit var  mypageViewModel : MyPageViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        mypageViewModel = ViewModelProvider(this).get(MyPageViewModel::class.java)
+
+        binding = FragmentMypageBinding.inflate(inflater, container, false)
+
+        //경험치 프로그레스 바
+        mypageViewModel.progress.observe(viewLifecycleOwner) { progress ->
+            binding.progressBar.progress = progress
+        }
+
+        //리사이클러뷰 Adapter
+
+
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
---
+++ app/src/main/java/com/example/potatoservice/ui/mypage/Volunteer.kt
@@ -0,0 +1,13 @@
+package com.example.potatoservice.ui.mypage
+
+data class Volunteer(
+    val title: String,
+    val institution: String,
+    val field: String,
+    val recruitmentPeriod: String,
+    val recruitmentCount: String,
+    val activityPeriod: String,
+    val volunteerHours: String,
+    val address: String,
+    val status: String
+)
