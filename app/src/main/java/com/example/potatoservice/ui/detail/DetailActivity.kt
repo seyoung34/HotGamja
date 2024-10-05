710a556c8e4eb9443fe49c0f6455fb9275e36cb7
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2024-10-05 22:37:30 +0900

    design: 상세 페이지 UI

--- app/src/main/java/com/example/potatoservice/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/potatoservice/ui/detail/DetailActivity.kt
@@ -1,10 +1,7 @@
 package com.example.potatoservice.ui.detail
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.example.potatoservice.R
 import com.example.potatoservice.databinding.ActivityDetailBinding
