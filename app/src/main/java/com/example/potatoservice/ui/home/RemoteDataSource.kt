64948a3c0e9713902d5fe0376337ff5f9df0a7a2
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2024-10-10 18:47:18 +0900

    feat: 봉사 활동 카테고리를 한글로 변경하는 함수 추가

--- app/src/main/java/com/example/potatoservice/ui/home/RemoteDataSource.kt
+++ app/src/main/java/com/example/potatoservice/ui/home/RemoteDataSource.kt
@@ -42,7 +42,7 @@ class RemoteDataSource @Inject constructor(private val apiService: APIService){
 								content.actStartTime,
 								content.actEndTime,
 								content.recruitTotalNum,
-								content.category
+								changeCategory(content.category)
 							)
 						}?: emptyList()
 						callback.onLoaded(activityList)
@@ -59,5 +59,18 @@ class RemoteDataSource @Inject constructor(private val apiService: APIService){
 
 			}
 		)
+	}
+
+	private fun changeCategory(originalCategory: String): String{
+		return when(originalCategory){
+			"LIFE_SUPPORT_AND_HOUSING_IMPROVEMENT" -> "생활지원 및 주거환경 개선"
+			"EDUCATION_AND_MENTORING" -> "교육 및 멘토링"
+			"ADMINISTRATIVE_AND_OFFICE_SUPPORT" -> "행정 및 사무지원"
+			"CULTURE_ENVIRONMENT_AND_INTERNATIONAL_COOPERATION" -> "문화, 환경 및 국제협력 활동"
+			"HEALTHCARE_AND_PUBLIC_WELFARE" -> "보건의료 및 공익활동"
+			"COUNSELING_AND_VOLUNTEER_TRAINING" -> "상담 및 자원봉사 교육"
+			"OTHER_ACTIVITIES" -> "기타 활동"
+			else -> "봉사 활동 분류 오류"
+		}
 	}
 }
