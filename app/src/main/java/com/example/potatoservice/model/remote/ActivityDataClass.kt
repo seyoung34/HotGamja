a8826c74e9e01d1607757044b55b0cd7f03feaea
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2024-10-10 19:41:16 +0900

    feat: 상세 페이지를 위한 ActivityDetail, Institute 데이터 클래스 생성

--- app/src/main/java/com/example/potatoservice/model/remote/ActivityDataClass.kt
+++ app/src/main/java/com/example/potatoservice/model/remote/ActivityDataClass.kt
@@ -40,3 +40,34 @@ data class Sort(
     val sorted: Boolean,
     val unsorted: Boolean
 )
+
+data class ActivityDetail(
+    val actId: Int,
+    val actTitle: String?,
+    val actLocation: String?,
+    val noticeStartDate: String?,
+    val noticeEndDate: String,
+    val actStartDate: String?,
+    val actEndDate: String,
+    val actStartTime: Int,
+    val actEndTime: Int,
+    val recruitTotalNum: Int,
+    val adultPossible: Boolean,
+    val teenPossible: Boolean,
+    val groupPossible: Boolean,
+    val actWeek: Int,
+    val actManager: String,
+    val actPhone: String,
+    val url: String,
+    val category: String,
+    val institute: Institute
+)
+
+data class Institute(
+    val instituteId:Int,
+    val name:String,
+    val location:String,
+    val latitude:Double,
+    val longitude:Double,
+    val phone:String
+)
